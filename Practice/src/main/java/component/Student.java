183c644a180daca6fb9bd7e0d19498a922658cb2
HazeCom <raibivek58@gmail.com>
2022-09-03 18:18:03 +0530

    practice

---
+++ Practice/src/main/java/component/Student.java
@@ -0,0 +1,62 @@
+package component;
+
+import javax.persistence.*;
+
+@Entity
+public class Student {
+	
+	@Id
+	private int roll;
+	private String name;
+	private String address;
+	private int marks;
+	
+	public Student() {
+	
+	}
+	
+	public Student(int id, String name, String address, int marks) {
+		this.roll = id;
+		this.name = name;
+		this.address = address;
+		this.marks = marks;
+	}
+	
+	public int getId() {
+		return roll;
+	}
+	
+	public void setId(int id) {
+		this.roll = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public int getMarks() {
+		return marks;
+	}
+	
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [Id=" + roll + ", name=" + name + ", address=" + address + ", marks=" + marks + "]";
+	}	
+	
+}
