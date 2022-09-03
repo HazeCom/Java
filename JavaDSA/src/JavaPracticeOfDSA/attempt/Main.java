aefea3a1fde9436587b3f76017c9c31dc2a7ed1a
HazeCom <raibivek58@gmail.com>
2022-09-03 21:08:11 +0530

    practice

---
+++ JavaDSA/src/JavaPracticeOfDSA/attempt/Main.java
@@ -0,0 +1,13 @@
+package JavaPracticeOfDSA.attempt;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+class Main{
+    static PrintWriter out = new PrintWriter(System.out);
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        
+    }
+}
